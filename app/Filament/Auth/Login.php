c4ad2f17fe44dfb1d932cecf9bdfc2c1c1bb8f86
afifihsnm <afifihsanke2@gmail.com>
2023-12-16 13:09:26 +0700

    feat(admin): Create Admin Panel

---
+++ app/Filament/Auth/Login.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use Filament\Pages\Auth\Login as BaseAuth;
+use Illuminate\Validation\ValidationException;
+
+class Login extends BaseAuth
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getLoginFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Name/Email')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL ) ? 'email' : 'name';
+
+        return [
+            $login_type => $data['login'],
+            'password'  => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+}
