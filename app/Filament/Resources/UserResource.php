6b03c6a0a6dae896b2e17554e3043fcc23d294f2
afifihsnm <afifihsanke2@gmail.com>
2023-12-22 14:10:58 +0700

    fix(ui): add label and tooltip

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama_lengkap')
+                    ->label('Full Name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('tanggal lahir')
+                    ->label('Date of Birth')
+                    ->required()
+                    ->maxDate(now()),
+                Forms\Components\TextInput::make('tempat_tinggal')
+                    ->label('Address')
+                    ->required(),
+                Forms\Components\TextInput::make('username')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->email(),
+                Forms\Components\TextInput::make('password')
+                    ->required()
+                    ->password(),
+                Forms\Components\Toggle::make('status')
+                    ->label('Account Status')
+                    ->required()
+                    ->onIcon('heroicon-s-no-symbol')
+                    ->offIcon('heroicon-s-no-symbol')
+                    ->onColor('success')
+                    ->offColor('danger')
+                    ->default(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Split::make([
+                    Tables\Columns\ImageColumn::make('avatar')
+                        ->disk('images')
+                        ->circular()
+                        ->grow(false),
+                    Stack::make([
+                        Tables\Columns\TextColumn::make('nama_lengkap')
+                            ->label('Full Name')
+                            ->weight(FontWeight::Bold)
+                            ->limit(24),
+                        Tables\Columns\TextColumn::make('username')
+                            ->searchable()
+                            ->limit(24),
+                    ]),
+                    Stack::make([
+                        Tables\Columns\TextColumn::make('email')
+                            ->icon('heroicon-m-envelope')
+                            ->searchable()
+                            ->limit(24),
+                        Tables\Columns\TextColumn::make('tanggal_lahir')
+                            ->label('Date of Birth')
+                            ->icon('heroicon-m-cake')
+                            ->sortable(),
+                    ]),
+                    Tables\Columns\TextColumn::make('tempat_tinggal')
+                        ->label('Address')
+                        ->icon('heroicon-s-home')
+                        ->limit(28),
+                    Tables\Columns\ToggleColumn::make('status')
+                        ->tooltip('Account Status')
+                        ->onIcon('heroicon-s-no-symbol')
+                        ->offIcon('heroicon-s-no-symbol')
+                        ->onColor('success')
+                        ->offColor('danger')
+                        ->grow(false)
+                        ->alignEnd()
+                ])
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                // Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            // 'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
