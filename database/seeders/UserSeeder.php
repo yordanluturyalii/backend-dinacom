ae90d649ff4add7543930d8728d1caaa23b8ae9b
afifihsnm <afifihsanke2@gmail.com>
2023-12-25 19:26:52 +0700

    feat(data): add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'nama_lengkap' => 'Hayo Siapaaa??',
+            'avatar' => 'images/avatar/avatar-dummy.jpg',
+            'tanggal_lahir' => '2009-12-31',
+            'tempat_tinggal' => 'Jl. Hmm..... Lupa wwkkwkw',
+            'username' => 'hayo.siapa_123',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('User123'),
+            'password_konfirmasi' => bcrypt('User123')
+        ]);
+    }
+}
