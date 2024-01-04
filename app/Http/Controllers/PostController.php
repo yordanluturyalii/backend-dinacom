64e45b152af9d8512482ef030329e87a6ea46751
jojoanakbaik <yordanluturyali@gmail.com>
2024-01-04 12:50:23 +0700

    fix(api): fix api response for share post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -164,17 +164,4 @@ class PostController extends Controller
             'message' => 'Berhasil Memberikan Tanggapan Cepat'
         ]);
     }
-
-    public function shareReport(): JsonResponse
-    {
-        $url = url()->current();
-
-        $json = [
-            'status' => 200,
-            'message' => 'Berhasil mengambil url',
-            'data' => $url
-        ];
-
-        return response()->json($json, 200);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -37,5 +37,4 @@ Route::group(['middleware' => 'api'], function () {
     Route::post('/v1/detail-report/{postId}/post/comment', [CommentController::class, 'store'])->name('post.comment');
     Route::post('/v1/detail-report/{postId}/comment/{commentId}', [CommentController::class, 'replyComment'])->name('reply.comment');
     Route::post('/v1/detail-report/{postId}/liked', [PostController::class,'giveLike'])->name('give.like');
-    Route::get('/v1/detail-report/{id}', [PostController::class, 'shareReport'])->name('share.report');
 });
