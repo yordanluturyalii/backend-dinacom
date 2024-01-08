ce637b9847d779430613acb3c15dee468e299cfc
jojoanakbaik <yordanluturyali@gmail.com>
2024-01-08 11:42:33 +0700

    fix(api): fix app url

--- app/Http/Resources/PostImageResource.php
+++ app/Http/Resources/PostImageResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use http\Env;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,7 +16,7 @@ class PostImageResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'path' => $this->path
+            'path' => \env("APP_URL").$this->path
         ];
     }
 }
