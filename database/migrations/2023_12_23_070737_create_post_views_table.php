7770148aafa941a1e8f2cbb3affc65c9eac76a40
afifihsnm <afifihsanke2@gmail.com>
2024-01-14 09:26:30 +0700

    fix(data): fix column to not unique

--- database/migrations/2023_12_23_070737_create_post_views_table.php
+++ database/migrations/2023_12_23_070737_create_post_views_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('post_views', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
-            $table->foreignId('post_id')->unique()->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
