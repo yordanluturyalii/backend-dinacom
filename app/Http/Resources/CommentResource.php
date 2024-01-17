e6a69311384bd742f39f166a9640c11dc892e61e
jojoanakbaik <yordanluturyali@gmail.com>
2024-01-17 13:43:21 +0700

    fix mboh

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -23,7 +23,7 @@ class CommentResource extends JsonResource
             'contentComment' => $this->content,
             'visibility' => $this->name_visibility,
             'parentId' => $this->parent_id,
-            'publishedAt' => Carbon::createFromFormat("d-m-Y H:i:s", $this->created_at, 'Asia/Jakarta')
+            'publishedAt' => Carbon::createFromFormat("d-m-Y H:i:s", Carbon::parse($this->created_at), 'Asia/Jakarta')
         ];
     }
 }
